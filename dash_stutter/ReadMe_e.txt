dd642c85130202855997f0b3488d6e3594cd57b6
Mohammed Moussa <moussa.uk@gmail.com>
2021-09-27 21:41:49 +0100

    Add DASH stutter repro

---
+++ dash_stutter/ReadMe_e.txt
@@ -0,0 +1,31 @@
+/* SIE CONFIDENTIAL PlayStation(R)5 Programmer Tool Runtime Library Release 1.00.00.26-00.00.00.0.1
+ * Copyright (C) 2019 Sony Interactive Entertainment Inc.
+ */
+
+========================================================================
+    Universal Video Player : "Video Test Sample" 
+========================================================================
+
+--------------------------------------
+DESCRIPTION
+
+This is a sample web application that can be loaded by the Web Launcher.
+
+How to customize
+----------------
+
+Sample Usage
+------------
+1. Copy the following files from the UVP library into the same directory as the sample
+    files.
+    - sce-uvp.esm.js
+    - sce-uvp.css
+    - sce-uvp-controls.css
+    - hls.js
+    - dash.js
+2. Host the files on a local web server and run the Web Launcher targeting
+    your host.
+   	If you have Node/npm installed, you run 'npm install' and 'npm start' to
+    perform this step.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ dash_stutter/index.js
@@ -0,0 +1,74 @@
+﻿import SceUvp from "./sce-uvp.esm.js"
+
+/**
+ * Helper function to load the application
+ */
+function loadApp() {
+
+  // Register the default dash library
+  SceUvp.use('dash');
+
+  // Create an instance of UVP
+  var player = new SceUvp.Player('#video-root', {
+    src: 'video/start.mpd',
+    muted: false,
+    autoplay: true,
+    controls: {
+      skipSec: {
+        forward: 5,
+        back: 5
+      }
+    },
+    fit: 'contain',
+    showControls: true,
+    useFullscreenControls: true,
+    isControllerTarget: true,
+    metadata: {
+      primaryText: 'movie title',
+      secondaryText: 'secondary text'
+    }
+  });
+
+  // Listen for any events
+  player.on('loadstart', (event) => {
+    console.log('Simple Sample - loadstart');
+  });
+
+  player.on('loadeddata', (event) => {
+    console.log('Simple Sample - loadeddata');
+
+    // Enable some logging in the dashjs player
+    player.adapter.player.updateSettings({
+      debug: {
+        logLevel: 5,
+      },
+    });
+  });
+
+  // Add the video player to the window so we can access it from the inspector
+  window.myUvpPlayer = player;
+};
+
+window.onload = function () {
+  SpatialNavigation.init();
+  SpatialNavigation.add({
+      selector: '.sce-uvp'
+  });
+  SpatialNavigation.makeFocusable();
+  SpatialNavigation.focus();
+}
+
+// Load the application once the page is ready
+if (['complete', 'loaded'].includes(document.readyState)) {
+  loadApp();
+} else {
+  document.addEventListener('DOMContentLoaded', loadApp);
+}
+
+// Wait for the MediaSDK to be initialized
+window.addEventListener('loadedmsdk', () => {
+  // Stop the system from Multi-tasking video or playing Background Music
+  window.msdk.device.disableSystemMedia().then((result) => {
+    console.log(`Disable System Media - result: ${result}`);
+  });
+});
